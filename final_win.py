6fafa71280d4f15f440b6fe42233263c22798770
ViskhanIsrapilov <israpilovv@gmail.com>
2025-01-04 12:40:01 +0300

    первая страница готова

--- final_win.py
+++ final_win.py
@@ -1,1 +1,4 @@
-# напиши здесь код третьего экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget,QLabel, QPushButton,QVBoxLayout,QHBoxLayout, QLineEdit
+from instr import *
+from final_win import *
--- second_win.py
+++ second_win.py
@@ -1,1 +1,4 @@
-# напиши здесь код для второго экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget,QLabel, QPushButton,QVBoxLayout,QHBoxLayout, QLineEdit
+from instr import *
+from second_win import *
